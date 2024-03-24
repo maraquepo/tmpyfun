89c08d76bc71b94f8cf80b808c2153fdab99f1f3
maraquepo <maraquepo@gmail.com>
2024-03-24 11:04:34 -0500

    full table

--- Frontend/TMMFun/src/components/UsersTable.vue
+++ Frontend/TMMFun/src/components/UsersTable.vue
@@ -1,54 +1,17 @@
-<template>
-  <div class="px-4 sm:px-6 lg:px-8">
-    <div class="mt-8 flow-root">
-      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead>
-              <tr
-                v-for="headerGroup in table.getHeaderGroups()"
-                :key="headerGroup.id"
-              >
-                <th
-                  v-for="header in headerGroup.headers"
-                  :key="header.id"
-                  scope="col"
-                  class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  <FlexRender
-                    :render="header.column.columnDef.header"
-                    :props="header.getContext()"
-                  />
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-              <tr v-for="row in table.getRowModel().rows" :key="row.id">
-                <td
-                  v-for="cell in row.getVisibleCells()"
-                  :key="cell.id"
-                  class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                >
-                  <FlexRender
-                    :render="cell.column.columnDef.cell"
-                    :props="cell.getContext()"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 // Make sure this block is also TypeScript
 import people from "../mockDataPeople.json";
-import { ref } from "vue";
-import { useVueTable, getCoreRowModel, FlexRender } from "@tanstack/vue-table";
+import { h, ref } from "vue";
+import {
+  useVueTable,
+  getCoreRowModel,
+  FlexRender,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+} from "@tanstack/vue-table";
 import { format } from "date-fns";
+import EditButton from "./EditButton.vue";
 
 const data = ref(people);
 
@@ -79,30 +42,30 @@ const columnsPeople = [
     accessorKey: "tokenBalance",
     header: "Token Balance",
   },
-  {
-    accessorKey: "public_user_id",
-    header: "Public User ID",
-  },
-  {
-    accessorKey: "creator_user_id", // Name
-    header: "Creator User ID",
-  },
-  {
-    accessorKey: "passwordhash",
-    header: "Password Hash",
-  },
+  // {
+  //   accessorKey: "public_user_id",
+  //   header: "Public User ID",
+  // },
+  // {
+  //   accessorKey: "creator_user_id", // Name
+  //   header: "Creator User ID",
+  // },
+  // {
+  //   accessorKey: "passwordhash",
+  //   header: "Password Hash",
+  // },
   {
     accessorKey: "signupIP",
     header: "Signup IP",
   },
-  {
-    accessorKey: "institution",
-    header: "Instituiton",
-  },
-  {
-    accessorKey: "bio",
-    header: "Bio",
-  },
+  // {
+  //   accessorKey: "institution",
+  //   header: "Instituiton",
+  // },
+  // {
+  //   accessorKey: "bio",
+  //   header: "Bio",
+  // },
   {
     accessorKey: "id",
     header: "ID",
@@ -110,13 +73,39 @@ const columnsPeople = [
   {
     accessorKey: "edit",
     header: " ",
+    cell: ({ row }) => h(EditButton, { id: row.original.id }),
   },
 ];
+
+const sorting = ref([]);
+const filter = ref("");
 
 const table = useVueTable({
   data: data.value,
   columns: columnsPeople,
   getCoreRowModel: getCoreRowModel(),
+  getPaginationRowModel: getPaginationRowModel(),
+  getSortedRowModel: getSortedRowModel(),
+  getFilteredRowModel: getFilteredRowModel(),
+  state: {
+    get sorting() {
+      return sorting.value;
+    },
+    get globalFilter() {
+      return filter.value;
+    },
+  },
+  onSortingChange: (updaterOrValue) => {
+    sorting.value =
+      typeof updaterOrValue === "function"
+        ? updaterOrValue(sorting.value)
+        : updaterOrValue;
+  },
+  // initialState: {
+  //   pagination: {
+  //     pageSize: 10,
+  //   },
+  // },
 });
 </script>
 
@@ -125,3 +114,115 @@ export default {
   name: "PeopleTable",
 };
 </script>
+
+<template>
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div class="my-4">
+            <input
+              type="text"
+              class="border border-gray-400 rounded px-2 py-2"
+              placeholder="Search"
+              v-model="filter"
+            />
+          </div>
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr
+                v-for="headerGroup in table.getHeaderGroups()"
+                :key="headerGroup.id"
+              >
+                <th
+                  v-for="header in headerGroup.headers"
+                  :key="header.id"
+                  scope="col"
+                  class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  :class="{
+                    'cursor-pointer select-none': header.column.getCanSort(),
+                  }"
+                  @click="header.column.getToggleSortingHandler()?.($event)"
+                >
+                  <FlexRender
+                    :render="header.column.columnDef.header"
+                    :props="header.getContext()"
+                  />
+                  {{ { asc: " ↑", desc: "↓" }[header.column.getIsSorted()] }}
+                </th>
+              </tr>
+            </thead>
+
+            <tbody class="divide-y divide-gray-200">
+              <tr v-for="row in table.getRowModel().rows" :key="row.id">
+                <td
+                  v-for="cell in row.getVisibleCells()"
+                  :key="cell.id"
+                  class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                >
+                  <FlexRender
+                    :render="cell.column.columnDef.cell"
+                    :props="cell.getContext()"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="mt-8">
+        Page {{ table.getState().pagination.pageIndex + 1 }} of
+        {{ table.getPageCount() }} -
+        {{ table.getFilteredRowModel().rows.length }} results
+      </div>
+      <div class="mt-8 space-x-4">
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="table.setPageSize(5)"
+        >
+          Page Size 5
+        </button>
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="table.setPageSize(10)"
+        >
+          Page Size 10
+        </button>
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="table.setPageSize(20)"
+        >
+          Page Size 20
+        </button>
+      </div>
+      <div class="space-x-4 mt-8">
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="table.setPageIndex(0)"
+        >
+          First page
+        </button>
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="table.setPageIndex(table.getPageCount() - 1)"
+        >
+          Last page
+        </button>
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          :disabled="!table.getCanPreviousPage()"
+          @click="table.previousPage()"
+        >
+          Prev page
+        </button>
+        <button
+          class="border border-gray-300 rounded px-2 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          :disabled="!table.getCanNextPage()"
+          @click="table.nextPage()"
+        >
+          Next page
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
