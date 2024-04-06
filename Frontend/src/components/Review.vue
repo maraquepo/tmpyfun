bb334eed78c914c81e7b07a9f09d99237b2af0b1
maraquepo <maraquepo@gmail.com>
2024-04-06 15:15:58 -0500

    review box

--- Frontend/src/components/Review.vue
+++ Frontend/src/components/Review.vue
@@ -1,5 +1,11 @@
 <script></script>
 
 <template>
-  <div class="flex justify-center text-red-300">Hello World</div>
+  <div class="my-20">
+    <div
+      class="flex justify-center items-center border-green-400 border rounded-md w-80 h-80"
+    >
+      <div class="text-green-400">Hello</div>
+    </div>
+  </div>
 </template>
